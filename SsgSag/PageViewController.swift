59ec651ae044b957ca56f0d6725efc5bd7043168
wndzlf <wndzlf@gmail.com>
2018-12-26 17:58:06 +0900

    aa

--- SsgSag/PageViewController.swift
+++ SsgSag/PageViewController.swift
@@ -8,13 +8,48 @@
 
 import UIKit
 
-class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource {
+class PageViewController: UIPageViewController, UIPageViewControllerDelegate, UIPageViewControllerDataSource, SegmentedProgressBarDelegate {
     
+    var SPB: SegmentedProgressBar!
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            self.SPB.startAnimation()
+        }
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        DispatchQueue.main.async {
+            self.SPB.currentAnimationIndex = 0
+            self.SPB.cancel()
+            self.SPB.isPaused = true
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.dataSource = self
         self.delegate = self
+        
+        
+        
+        SPB = SegmentedProgressBar(numberOfSegments: 2)
+        if #available(iOS 11.0, *) {
+            SPB.frame = CGRect(x: 18, y: UIApplication.shared.statusBarFrame.height + 5, width: view.frame.width - 35, height: 3)
+        } else {
+            // Fallback on earlier versions
+            SPB.frame = CGRect(x: 18, y: 15, width: view.frame.width - 35, height: 3)
+        }
+        
+        SPB.delegate = self
+        SPB.topColor = UIColor.white
+        SPB.bottomColor = UIColor.white.withAlphaComponent(0.25)
+        SPB.padding = 2
+        SPB.isPaused = true
+        SPB.currentAnimationIndex = 0
+        //        SPB.duration = getDuration(at: 0)
+        view.addSubview(SPB)
+        view.bringSubview(toFront: SPB)
         
         
         
