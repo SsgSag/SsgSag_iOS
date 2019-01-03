dedb614a29c6870d19e840c0995132807fdcbf88
wndzlf <wndzlf@gmail.com>
2019-01-04 00:01:52 +0900

    야밤에 부탁하신거 커밋합니다^_^

--- SsgSag/Calendar/CalenderVC.swift
+++ SsgSag/Calendar/CalenderVC.swift
@@ -31,7 +31,7 @@ class CalenderVC: UIViewController{
     
     let todoUpDownView: UIView = {
         let todoView = UIView()
-        todoView.backgroundColor = .yellow
+        todoView.backgroundColor = UIColor(displayP3Red: 251/255, green: 251/255, blue: 251/255, alpha: 1.0)
         todoView.translatesAutoresizingMaskIntoConstraints = false
         return todoView
     }()
@@ -42,8 +42,11 @@ class CalenderVC: UIViewController{
         return todo
     }()
     
+    let todoExampleDate = ["1","2","3","4","5","6"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         let todoSwipeUp = UISwipeGestureRecognizer(target: self, action: #selector(todoUp))
         let todoSwipeDown = UITapGestureRecognizer(target: self, action: #selector(todoDown))
         let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(leftSwipeAction))
@@ -65,6 +68,11 @@ class CalenderVC: UIViewController{
         todoTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
         todoTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
         todoTableView.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.25).isActive = true
+        //todoTableView.separatorStyle = .none
+        todoTableView.rowHeight = view.frame.height / 13
+        todoTableView.dataSource = self
+        todoTableView.delegate = self
+        todoTableView.register(todoCell.self, forCellReuseIdentifier: "todoCell")
         
         view.addSubview(todoUpDownView)
         todoUpDownView.bottomAnchor.constraint(equalTo: todoTableView.topAnchor).isActive = true
@@ -135,3 +143,78 @@ class CalenderVC: UIViewController{
     }
 }
 
+
+extension CalenderVC: UITableViewDelegate {
+    
+}
+
+extension CalenderVC: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.todoExampleDate.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "todoCell") as! todoCell
+        
+        return cell
+    }
+    
+}
+
+class todoCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.backgroundColor = UIColor(displayP3Red: 251/255, green: 251/255, blue: 251/255, alpha: 1.0)
+        setupCell()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //setupCell()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let borderView: UIView = {
+        let bV = UIView()
+        bV.layer.cornerRadius = 15
+        bV.layer.masksToBounds = true
+        bV.backgroundColor = .lightGray
+        bV.translatesAutoresizingMaskIntoConstraints = false
+        return bV
+    }()
+    
+    let leftLineView: UIView = {
+        let leftView = UIView()
+        leftView.roundCorners(corners: [.topLeft, .bottomLeft], radius: 15)
+        leftView.translatesAutoresizingMaskIntoConstraints = false
+        leftView.backgroundColor = UIColor(displayP3Red: 97/255, green: 118/255, blue: 221/255, alpha: 1.0)
+        return leftView
+    }()
+    
+    func setupCell(){
+        addSubview(borderView)
+        borderView.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
+        borderView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 10).isActive = true
+        borderView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -10 ).isActive = true
+        borderView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -10).isActive = true
+        
+        addSubview(leftLineView)
+        leftLineView.leftAnchor.constraint(equalTo: borderView.leftAnchor).isActive = true
+        leftLineView.topAnchor.constraint(equalTo: borderView.topAnchor).isActive = true
+        leftLineView.bottomAnchor.constraint(equalTo: borderView.bottomAnchor).isActive = true
+        leftLineView.widthAnchor.constraint(equalToConstant: 7).isActive = true
+    }
+}
+
+extension UIView {
+    func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
