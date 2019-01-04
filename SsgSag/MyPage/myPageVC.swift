547a5820716d340215b72131c5feb26318090d7e
CHOMINJI <>
2019-01-04 20:54:24 +0900

    create mypage stroyboard
    관심분야, 구직여부
    구직여부 이미지 변경 필요

--- SsgSag/MyPage/myPageVC.swift
+++ SsgSag/MyPage/myPageVC.swift
@@ -9,11 +9,24 @@
 import UIKit
 
 class myPageVC: UIViewController {
-
+    
+//    @IBOutlet weak var divisionLineView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+//        var gradient = CAGradientLayer()
+//
+//        gradient.startPoint = CGPoint(x: 0, y: 0.5)
+//        gradient.endPoint = CGPoint(x: 1, y: 0.5)
+//        gradient.colors = [
+//            UIColor.rgb(red: 65, green: 163, blue: 255),
+//            UIColor.rgb(red: 155, green: 65, blue: 250)
+//        ]
+//        self.divisionLineView.layer.addSublayer(gradient)
+        
     }
     
 
