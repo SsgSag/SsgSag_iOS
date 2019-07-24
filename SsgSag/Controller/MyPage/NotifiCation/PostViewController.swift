445ab60a76200c8c45dea7125a693b648e59496a
hyeju <hyeju.tech@gmail.com>
2019-07-24 14:22:21 +0900

    FEAT: notice html 형식에 맞게 띄우도록 수정
    #64
    webView를 사용해 html형식에 맞게 띄워지도록 함

--- SsgSag/Controller/MyPage/NotifiCation/PostViewController.swift
+++ SsgSag/Controller/MyPage/NotifiCation/PostViewController.swift
@@ -7,25 +7,18 @@
 //
 
 import UIKit
+import WebKit
 
-class PostViewController: UIViewController {
+class PostViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
 
     var noticeData: NoticeData?
     
-//    private let contentsTextView: UITextView = {
-//        let textView = UITextView()
-//        textView.translatesAutoresizingMaskIntoConstraints = false
-//        textView.font = UIFont.systemFont(ofSize: 14)
-//        textView.textColor = #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1)
-//        return textView
-//    }()
-//
-    private lazy var postTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.delegate = self
-        tableView.dataSource = self
-        return tableView
+    private lazy var noticeWebView: WKWebView = {
+        let webView = WKWebView()
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        return webView
     }()
     
     private lazy var backbutton = UIBarButtonItem(image: UIImage(named: "ic_ArrowBack"),
@@ -41,54 +34,20 @@ class PostViewController: UIViewController {
         navigationItem.title = noticeData?.noticeName
     }
     
+    override func loadView() {
+        super.loadView()
+        
+        self.view = self.noticeWebView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        setupLayout()
-        setupTableView()
-    }
-    
-    private func setupLayout() {
-        view.backgroundColor = .white
         
-        view.addSubview(postTableView)
-        
-        postTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
-        postTableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
-        postTableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
-        postTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
-    }
-    
-    private func setupTableView() {
-        postTableView.register(PostTableViewCell.self, forCellReuseIdentifier: "postCellID")
+        noticeWebView.loadHTMLString(noticeData?.noticeContent ?? "", baseURL: nil)
     }
     
     @objc private func touchUpBackButton() {
         navigationController?.popViewController(animated: true)
     }
 
-}
-
-extension PostViewController: UITableViewDelegate {
-}
-
-extension PostViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "postCellID",
-                                                       for: indexPath) as? PostTableViewCell else {
-            return .init()
-        }
-        
-        if indexPath.row == 0 {
-            cell.setContentsLabel(color: #colorLiteral(red: 0.1921568627, green: 0.1921568627, blue: 0.1921568627, alpha: 1), size: 14, text: "asdf")
-        } else {
-            cell.setContentsLabel(color: #colorLiteral(red: 0.7411764706, green: 0.7411764706, blue: 0.7411764706, alpha: 1), size: 13, text: "asdf")
-        }
-        return cell
-    }
 }
