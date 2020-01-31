9cfe1df6be07a03dd2232ba871ab89394a3ecc7e
namsoo5 <are34@naver.com>
2020-01-31 19:00:42 +0900

    UPDATE: ReviewSearchView 생성 대략적 디자인구현
    - 테이블 셀 미구현

---
+++ SsgSag/Screens/Review/Views/Cell/ReviewSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReviewSearchTableViewCell.swift
+//  SsgSag
+//
+//  Created by 남수김 on 2020/01/31.
+//  Copyright © 2020 wndzlf. All rights reserved.
+//
+
+import UIKit
+
+class ReviewSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ SsgSag/Screens/Review/Views/ReviewSearchViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ReviewSearchViewController.swift
+//  SsgSag
+//
+//  Created by 남수김 on 2020/01/31.
+//  Copyright © 2020 wndzlf. All rights reserved.
+//
+
+import UIKit
+
+class ReviewSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
+
+// 동아리 등록하기버튼 위로 보이게하기
+// 카테고리 스텍뷰만들기
