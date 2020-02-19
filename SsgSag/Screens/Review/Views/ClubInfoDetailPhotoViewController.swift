fcd87915201acbd5b6568967b349fafd376599c6
namsoo5 <are34@naver.com>
2020-02-19 11:04:46 +0900

    UPDATE: 동아리 상세 정보탭 사진뷰 기능구현
    - 페이지에따른 라벨 변화
    - 터치시 UI숨기기/보이기

--- SsgSag/Screens/Review/Views/ClubInfoDetailPhotoViewController.swift
+++ SsgSag/Screens/Review/Views/ClubInfoDetailPhotoViewController.swift
@@ -12,11 +12,13 @@ import RxSwift
 
 class ClubInfoDetailPhotoViewController: UIViewController {
 
+    @IBOutlet weak var cancelButton: UIButton!
     @IBOutlet weak var photoCountLabel: UILabel!
     @IBOutlet weak var collectionView: UICollectionView!
     
-    var photoURLSet = BehaviorRelay<[String]>(value: [])
-    var photoCurIndex = BehaviorRelay<Int>(value: -1)
+    let photoURLSet = BehaviorRelay<[String]>(value: [])
+    let photoCurIndex = BehaviorRelay<Int>(value: -1)
+    let isHiddenTopUIObservable = BehaviorRelay(value: true)
     let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
@@ -26,11 +28,29 @@ class ClubInfoDetailPhotoViewController: UIViewController {
         let indexPath = IndexPath(item: photoCurIndex.value, section: 0)
         collectionView.scrollToItem(at: indexPath, at: .left, animated: false)
         
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(isHiddenUI))
+        collectionView.addGestureRecognizer(tapRecognizer)
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isHiddenUI()
+    }
+    
+    @objc func isHiddenUI() {
+        let isHidden = isHiddenTopUIObservable.value
+        isHiddenTopUIObservable.accept(!isHidden)
     }
     
     func collectionViewSet() {
         let nib = UINib(nibName: "ClubInfoDetailPhotoCollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: "ClubInfoDetailPhotoCell")
+        
+        collectionView.delegate = self
+        collectionView.minimumZoomScale = 1
+        collectionView.maximumZoomScale = 3
+        
+        collectionView.decelerationRate = .fast
         
         photoURLSet
             .bind(to: collectionView.rx.items(cellIdentifier: "ClubInfoDetailPhotoCell")) { [weak self] (indexPath, cellViewModel, cell)  in
@@ -50,15 +70,25 @@ class ClubInfoDetailPhotoViewController: UIViewController {
                 self?.photoCurIndex.accept(page)
             })
             .disposed(by: disposeBag)
+        
     }
     
     func bind() {
         photoCurIndex
+            .asDriver()
             .distinctUntilChanged()
             .filter{ $0 >= 0 }
-            .subscribe(onNext: { [weak self] page in
+            .drive(onNext: { [weak self] page in
                 guard let photoCount = self?.photoURLSet.value.count else {return}
                 self?.photoCountLabel.text = "\(page+1)/\(photoCount)"
+            })
+            .disposed(by: disposeBag)
+        
+        isHiddenTopUIObservable
+            .asDriver()
+            .drive(onNext: { [weak self] isHidden in
+                self?.photoCountLabel.isHidden = isHidden
+                self?.cancelButton.isHidden = isHidden
             })
             .disposed(by: disposeBag)
     }
@@ -67,3 +97,24 @@ class ClubInfoDetailPhotoViewController: UIViewController {
         self.dismiss(animated: true)
     }
 }
+
+extension ClubInfoDetailPhotoViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = collectionView.frame.width
+        let height = width
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+extension ClubInfoDetailPhotoViewController: UICollectionViewDelegate {
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+    
+        let offset = targetContentOffset.pointee
+        let cellWidthIncludingSpacing = self.collectionView.frame.width
+        let index = Int(offset.x / cellWidthIncludingSpacing)
+        
+        photoCurIndex.accept(index)
+    }
+}
+
