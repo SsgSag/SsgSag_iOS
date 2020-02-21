61188639b133be5b6381689477e94ea857d01d3e
namsoo5 <are34@naver.com>
2020-02-21 14:15:37 +0900

    UPDATE: 사진보기시 statusBar white적용

--- SsgSag/Screens/Review/Views/ClubInfoDetailPhotoViewController.swift
+++ SsgSag/Screens/Review/Views/ClubInfoDetailPhotoViewController.swift
@@ -33,6 +33,10 @@ class ClubInfoDetailPhotoViewController: UIViewController {
         
     }
     
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         isHiddenUI()
     }
