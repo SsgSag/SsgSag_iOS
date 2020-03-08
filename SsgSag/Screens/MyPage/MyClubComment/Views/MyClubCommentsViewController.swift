2d9701c3beb57f11494bd48695c6edf2cad5a66c
sangbumGoh <kl9151@naver.com>
2020-03-09 02:34:36 +0900

    UPDATE: 나의후기 비엇을 경우 로직추가

--- SsgSag/Screens/MyPage/MyClubComment/Views/MyClubCommentsViewController.swift
+++ SsgSag/Screens/MyPage/MyClubComment/Views/MyClubCommentsViewController.swift
@@ -11,6 +11,16 @@ import RxSwift
 import RxCocoa
 
 class MyClubCommentsViewController: UIViewController {
+    
+    private let emptyViewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "내가 쓴 후기가 없네요 :("
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 15, weight: .semibold)
+        return label
+    }()
+    
     var disposeBag = DisposeBag()
     var viewModel: MyClubViewModel?
     @IBOutlet weak var myClubTableView: UITableView!
@@ -21,15 +31,20 @@ class MyClubCommentsViewController: UIViewController {
         let viewModel = MyClubViewModel()
         self.viewModel = viewModel
         bind(viewModel: viewModel)
-        myClubTableView
-            .rx
-            .setDelegate(self)
-            .disposed(by: disposeBag)
+        myClubTableView.delegate = self
+        myClubTableView.dataSource = self
     }
     
     func bind(viewModel: MyClubViewModel) {
         viewModel.buildCellViewModels()
         
+        viewModel
+            .commentCellViewModels
+            .asDriver()
+            .drive(onNext: { [weak self] _ in
+                self?.myClubTableView.reloadData()
+            })
+            .disposed(by: disposeBag)
         navigationBackButton
             .rx
             .tap
@@ -37,15 +52,6 @@ class MyClubCommentsViewController: UIViewController {
                 self?.navigationController?.popViewController(animated: true)
             })
             .disposed(by: disposeBag)
-        
-        
-        viewModel.commentCellViewModels.asObservable()
-            .bind(to: myClubTableView.rx.items(cellIdentifier: "MyClubTableViewCell")) { (indexPath, cellViewModel, cell)  in
-                guard let cell = cell as? MyClubTableViewCell else { return }
-                cell.bind(viewModel: cellViewModel)
-        }
-        .disposed(by: disposeBag)
-        
         
         myClubTableView
             .rx
@@ -68,5 +74,47 @@ extension MyClubCommentsViewController: UITableViewDelegate {
                    heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 88
     }
+    
+    func tableView(_ tableView: UITableView,
+                   heightForFooterInSection section: Int) -> CGFloat {
+        if viewModel?.commentCellViewModels.value.isEmpty ?? true {
+            return 100
+        } else {
+            return 0
+        }
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
+    }
 }
 
+extension MyClubCommentsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.commentCellViewModels.value.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MyClubTableViewCell") as? MyClubTableViewCell, let cellViewModel = viewModel?.commentCellViewModels.value[safe: indexPath.row] else { return .init() }
+        cell.bind(viewModel: cellViewModel)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView,
+                   viewForFooterInSection section: Int) -> UIView? {
+        if viewModel?.commentCellViewModels.value.isEmpty ?? true {
+            emptyViewLabel.frame = .init(x: 0,
+                                         y: 0,
+                                         width: tableView.frame.width,
+                                         height: 100)
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: tableView.frame.size.width, height: 50))
+            view.addSubview(emptyViewLabel)
+            return view
+        } else {
+            return nil
+        }
+    }
+}
